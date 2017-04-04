30d982a0d6893497cd6722c4bfcb4354215dbd91
Gihun Ham <ham@e-mirim.hs.kr>
2017-04-04 20:15:45 +0900

    새로운 프로젝트 - 유리한

---
+++ app/src/main/java/kr/hs/emirim/progh2/friendsimulator/hanyuri/MainActivity.java
@@ -0,0 +1,13 @@
+package kr.hs.emirim.progh2.friendsimulator.hanyuri;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
