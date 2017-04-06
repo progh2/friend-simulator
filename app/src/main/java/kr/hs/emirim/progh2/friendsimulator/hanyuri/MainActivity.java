db8d245c833404746309edd3b078623a372ad3e2
Gihun Ham <ham@e-mirim.hs.kr>
2017-04-06 20:54:06 +0900

    #10 로그 남기기

--- app/src/main/java/kr/hs/emirim/progh2/friendsimulator/hanyuri/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/progh2/friendsimulator/hanyuri/MainActivity.java
@@ -2,39 +2,51 @@ package kr.hs.emirim.progh2.friendsimulator.hanyuri;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public final static String TAG = "한유리:Main";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate 메소드 호출~");
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "activity_main 레이아웃 세팅~");
     }
 
     void onClick( View view ){
+        Log.d(TAG, "onClick 메소드 호출~");
+        Log.d(TAG, "클릭된 뷰 id:" + view.getId() );
         switch( view.getId() ){
             case R.id.button1:
+                Log.d(TAG, "버튼1 클릭");
                 Toast.makeText(this, "인냐~~~~~?", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.button2:
+                Log.d(TAG, "버튼2 클릭");
                 Toast.makeText(this, "야 디질래?", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.button3:
+                Log.d(TAG, "버튼3 클릭");
                 Toast.makeText(this, "이러나~", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.button4:
+                Log.d(TAG, "버튼4 클릭");
                 Toast.makeText(this, "헛쏘리하지마", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.button5:
+                Log.e(TAG, "버튼5 클릭");
                 Toast.makeText(this, "우하하하하핳ㅎ하하", Toast.LENGTH_SHORT).show();
                 break;
             default:
+                Log.e(TAG, "모르는 녀석 클릭");
                 Toast.makeText(this, "뭐라하는지 모르겠어용", Toast.LENGTH_SHORT).show();
                 break;
         }
-
+        Log.d(TAG, "onClick 메소드 무사히 종료");
 
 
     }
